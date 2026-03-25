73b8ebd11151ad1cb95c75b493af56ab903301ff
gnozea <gerson.nozea@onelink4dev.com>
2026-03-25 10:07:04 -0500

    Add Equipment List placeholder

--- src/src/components/src/components/src/pages/src/pages/src/pages/EquipmentDetail.tsx
+++ src/src/components/src/components/src/pages/src/pages/src/pages/EquipmentDetail.tsx
@@ -1,1 +1,8 @@
-// placeholder
+export default function EquipmentDetail() {
+  return (
+    <div>
+      <h1>Equipment Detail</h1>
+      <p>This is the equipment detail page.</p>
+    </div>
+  );
+}
